11287c0624deaa6deff73bbe28ce14b81e962849
Hamid-Hassani <hamidhassani119@gmail.com>
2025-09-13 14:51:16 +0330

    feat/Create appLayout

--- src/App.jsx
+++ src/App.jsx
@@ -4,32 +4,39 @@ import Menu from "./features/menu/Menu";
 import Cart from "./features/cart/Cart";
 import CreateOrder from "./features/order/CreateOrder";
 import Order from "./features/order/Order";
+import LayoutApp from "./ui/LayoutApp";
 
 const router = createBrowserRouter([
   {
-    path: "/",
-    element: <Home />,
-  },
-  {
-    path: "/menu",
-    element: <Menu />,
-  },
-  {
-    path: "/cart",
-    element: <Cart />,
-  },
-  {
-    path: "/order/new",
-    element: <CreateOrder />,
-  },
-  {
-    path: "/order/:orderId",
-    element: <Order />,
+    element: <LayoutApp />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/menu",
+        element: <Menu />,
+      },
+      {
+        path: "/cart",
+        element: <Cart />,
+      },
+      {
+        path: "/order/new",
+        element: <CreateOrder />,
+      },
+      {
+        path: "/order/:orderId",
+        element: <Order />,
+      },
+    ],
   },
 ]);
 
-function App({ children }) {
-  return <RouterProvider router={router}>{children}</RouterProvider>;
+function App() {
+  return <RouterProvider router={router} />;
 }
 
 export default App;
+``;
